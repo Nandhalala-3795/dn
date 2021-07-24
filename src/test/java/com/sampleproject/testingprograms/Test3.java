c6f36b82196b6e3954471f159e73264f16e8e695
nandh <nandh@LAPTOP-71H19J9P>
2021-07-24 14:58:22 +0530

    second

---
+++ src/test/java/com/sampleproject/testingprograms/Test3.java
@@ -0,0 +1,13 @@
+package com.sampleproject.testingprograms;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test3 
+{
+	@Test
+	public void tc3()
+	{
+		Reporter.log("test3",true);
+	}
+}
